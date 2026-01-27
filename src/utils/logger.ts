ec8f2f4738b0fe5fe594b29be3296513586deb42
hawdyx <--global>
2026-01-28 03:06:53 +0530

    feat: 3

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -7,19 +7,23 @@ export class Logger {
     this.prefix = prefix;
   }
 
+  private getTimestamp(): string {
+    return new Date().toISOString().split("T")[1].slice(0, 8);
+  }
+
   error(msg: string): void {
-    console.log(styleText("red", `[${this.prefix}] ERROR: ${msg}`));
+    console.log(styleText("red", `[${this.prefix}] [${this.getTimestamp()}] ERROR: ${msg}`));
   }
 
   success(msg: string): void {
-    console.log(styleText("green", `[${this.prefix}] SUCCESS: ${msg}`));
+    console.log(styleText("green", `[${this.prefix}] [${this.getTimestamp()}] SUCCESS: ${msg}`));
   }
 
   info(msg: string): void {
-    console.log(styleText("blue", `[${this.prefix}] INFO: ${msg}`));
+    console.log(styleText("blue", `[${this.prefix}] [${this.getTimestamp()}] INFO: ${msg}`));
   }
 
   warn(msg: string): void {
-    console.log(styleText("yellow", `[${this.prefix}] WARN: ${msg}`));
+    console.log(styleText("yellow", `[${this.prefix}] [${this.getTimestamp()}] WARN: ${msg}`));
   }
 }
