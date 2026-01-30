bb41d9ffa0b4731d058df6cc4f73852de338fb7e
hawdyx <irushiperera2006@gmail.com>
2026-01-31 02:44:31 +0530

    a

--- src/helpers/scriptThree.ts
+++ src/helpers/scriptThree.ts
@@ -1,7 +1,10 @@
 import chalk from "chalk";
+import { kill } from "node:process";
 
 cbnsole.log(chalk.cyan("chicken run"));
 cnsole.log(chalk.cyan("chicken run"));
 console.log(chalk.white("chicken run"));
 console.log(chalk.cyan("chicken run"));
 bla bla
+bla bla
+jj
