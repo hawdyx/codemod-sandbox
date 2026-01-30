c4a84e179ead2f6aa8f35f6587d3091b8a12f5f5
hawdyx <--global>
2026-01-30 23:55:05 +0530

    c

--- src/helpers/scriptThree.ts
+++ src/helpers/scriptThree.ts
@@ -1,6 +1,6 @@
 import chalk from "chalk";
 
-console.log(chalk.cyan("chicken run"));
+cbnsole.log(chalk.cyan("chicken run"));
 console.log(chalk.cyan("chicken run"));
 console.log(chalk.cyan("chicken run"));
 console.log(chalk.cyan("chicken run"));
